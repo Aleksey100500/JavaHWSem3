b1bb837739c95d540a83ff2b3a130dd64b0208df
Aleksey <a_zhuravlyov@bk.ru>
2023-02-01 23:41:04 +0300

    Finished HW

--- src/main/java/JavaS3HW.java
+++ src/main/java/JavaS3HW.java
@@ -1,14 +1,13 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class JavaS3HW {
     public static void main(String[] args) {
         //1. Создать новый список, добавить несколько строк и вывести коллекцию на экран.
-        String str1 = "Hello!";
-        String str2 = "HelloWorld!";
-        String str3 = "HelloWorldJava!";
-        String str4 = "Hello!";
+
+        String str1 = "Yellow";
+        String str2 = "Blue";
+        String str3 = "Red";
+        String str4 = "Orange";
 
         List<String> list = new ArrayList<>();
         list.add(str1);
@@ -16,11 +15,60 @@ public class JavaS3HW {
         list.add(str3);
         list.add(str4);
 
-        Iterator<String> iterator = list.iterator();
-        while (iterator.hasNext()){
-            String str = iterator.next();
-            System.out.println(str);
-        }
+        System.out.println(list);
 
+        // 2.Итерация всех элементов списка цветов и добавления к каждому символа '!'.
+
+        String symbol = "!";
+        String temp;
+        List<String> list1 = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            temp = list.get(i);
+            temp = temp + symbol;
+            list1.add(temp);
+        }
+        System.out.println(list1);
+
+        // 3. Вставить элемент в список в первой позиции.
+
+        list1.add(0, "Green!");
+        System.out.println(list1);
+
+        // 4. Извлечь элемент (по указанному индексу) из заданного списка.
+
+        System.out.println(list1.get(0));
+
+        // 5. Обновить определенный элемент списка по заданному индексу.
+
+        list1.set(3,"Brown!");
+        System.out.println(list1);
+
+        // 6. Удалить третий элемент из списка.
+
+        list1.remove(2);
+        System.out.println(list1);
+
+        // 7. Поиска элемента в списке по строке.
+
+        System.out.println("Its index: " + list1.indexOf("Orange!"));
+
+        // 8. Создать новый список и добавить в него несколько елементов первого списка.
+
+        Random rnd = new Random();
+        List<String> list2 = new ArrayList<>();
+        for (int i = 0; i < rnd.nextInt(1, 3); i++) {
+            list2.add(list1.get(i));
+        }
+        System.out.println("Its a new list: " + list2);
+
+        // 9. Удалить из первого списка все элементы отсутствующие во втором списке.
+
+        list1.removeAll(list2);
+        System.out.println(list1);
+
+        // 10. *Сортировка списка.
+
+        Collections.sort(list1);
+        System.out.println(list1);
     }
 }
