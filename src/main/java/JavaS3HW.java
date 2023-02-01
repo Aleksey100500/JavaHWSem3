a6ef8fb0bc7962be853d04892b082e7156cd820f
Aleksey <a_zhuravlyov@bk.ru>
2023-02-01 21:02:59 +0300

    Finished first exercise

--- src/main/java/JavaS3HW.java
+++ src/main/java/JavaS3HW.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class JavaS3HW {
     public static void main(String[] args) {
+        //1. Создать новый список, добавить несколько строк и вывести коллекцию на экран.
+        String str1 = "Hello!";
+        String str2 = "HelloWorld!";
+        String str3 = "HelloWorldJava!";
+        String str4 = "Hello!";
+
+        List<String> list = new ArrayList<>();
+        list.add(str1);
+        list.add(str2);
+        list.add(str3);
+        list.add(str4);
+
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            String str = iterator.next();
+            System.out.println(str);
+        }
 
     }
 }
